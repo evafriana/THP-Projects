3e4c4ccc2876be5cdd15c03fec996f6a3d72ae7a
Eva Afriana <vava.afriana@gmail.com>
2020-04-06 15:47:59 +0200

    exo_ruby_2.9

---
+++ 20200407_first_exo_ruby/exo_09.rb
@@ -0,0 +1,10 @@
+puts "Bonjour, c'est quoi ton prénom ?"
+print "> "
+surname = gets.chomp
+
+puts "Bonjour, c'est quoi ton nom ?"
+print "> "
+name = gets.chomp
+
+
+puts "Bonjour, #{surname.capitalize} #{name.capitalize}"
