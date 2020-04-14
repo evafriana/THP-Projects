8f747783c5a2c7a8160914e5cfb5865b8afca881
Eva Afriana <vava.afriana@gmail.com>
2020-04-15 00:05:03 +0200

    Update day_trader.rb

--- 20200414_ASCII_Ruby/lib/day_trader.rb
+++ 20200414_ASCII_Ruby/lib/day_trader.rb
@@ -1,12 +1,12 @@
 def day_trader(array)
-  result = []
-  bought = 0
-  sold = 0
-  best = 0
-  current = 0
-  array.each_with_index do |x, i|
-    y = 0
-    while y < i
+ result = []
+ bought = 0
+ sold = 0
+ best = 0
+ current = 0
+ array.each_with_index do |x, i|
+ y = 0
+  while y < i
     current = x - array[y]
     bought = array[y] if best < current
     sold = x if best < current
@@ -14,7 +14,9 @@ def day_trader(array)
     y += 1
     end
   end
+
   result << array.index(bought)
   result << array.index(sold)
   return result
+
 end
